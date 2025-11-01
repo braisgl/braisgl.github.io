c3c4983f468d2fb6d9cb3964cc2ccf1d5885fb49
Brais García Lapido <braisgl.dev@gmail.com>
2025-11-01 19:25:10 +0100

    feat: añadido la banda a nosotros

--- js/config.youtube.js
+++ js/config.youtube.js
@@ -1,6 +1,6 @@
 window.YT_CONFIG = window.YT_CONFIG || {
- apiKey: '', // p.ej. 'AIzaSy...'
-  channelId: 'UCMRGk5iv_2V4yBeK1gs6_RQ', // Tu channelId público (está bien publicarlo)
+ apiKey: '',
+  channelId: 'UCMRGk5iv_2V4yBeK1gs6_RQ',
   apiBase: 'https://calm-heart-dbb5.daydreamssmusic.workers.dev/api',
   privacyEnhanced: false,
 };
